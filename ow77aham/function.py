63f6e3cfc17af82f0d08936b2773e0c55499fc88
ow77aham <quirin.bufler@fau.de>
2021-11-14 22:51:31 +0100

    comments: committing my first package second try

--- ow77aham/function.py
+++ ow77aham/function.py
@@ -7,7 +7,7 @@ def imshow(X, resize=None):
     You should create a way to resize an image from an array X. 
     The use of widgets is optional but you can take a look to interact.
     We should be able to install this package in Google Colab from your Git 
-repo. 
+    repo. 
     """
-    print('test')
+    print('test2')
     pass
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from setuptools import find_packages
 setup(name='ow77aham',
-      version='0.1',
+      version='1.0',
       author='DSSS',
       author_email='quirin.bufler@fau.de',
       packages=find_packages(),
