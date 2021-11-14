c73055a68b11d77834c7ccef76e8033610d81aaa
ow77aham <quirin.bufler@fau.de>
2021-11-15 00:12:03 +0100

    comments: committing ex3

--- ow77aham/function.py
+++ ow77aham/function.py
@@ -3,11 +3,6 @@ from ipywidgets import interact, fixed
 from PIL import Image
     
 def imshow(X, resize=None):
-    """
-    You should create a way to resize an image from an array X. 
-    The use of widgets is optional but you can take a look to interact.
-    We should be able to install this package in Google Colab from your Git 
-    repo. 
-    """
-    print('test2')
+    X = np.resize(X, resize)
+    plt.imshow(X)
     pass
