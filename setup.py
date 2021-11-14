171fa9d34a8ea3c4ba458c8cdfa7ef3f4e9edf8a
ow77aham <quirin.bufler@fau.de>
2021-11-14 22:24:55 +0100

    comments: committing my first package

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(name='ow77aham',
+      version='0.1',
+      author='DSSS',
+      author_email='quirin.bufler@fau.de',
+      packages=find_packages(),
+      install_requires=['numpy', 'Pillow', 'ipywidgets'])
